8a4431ea22af33bc0de0f0308084d71a9aea8a0c
Vinayak Gupta <guptavinayakg@gmail.com>
2021-03-20 01:39:12 +0530

    Registration Form Connect

--- src/components/RegisterSection/RegisterSection.js
+++ src/components/RegisterSection/RegisterSection.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import WindowBar from "../WindowBar/WindowBar";
-
+import axios from "axios";
+import { useGoogleReCaptcha } from "react-google-recaptcha-v3";
 import "./RegisterSection.css";
 
 const RegisterSection = () => {
@@ -10,16 +11,37 @@ const RegisterSection = () => {
 	const [college, setCollege] = useState("");
 	const [reg, setReg] = useState("");
 
-	const submit = () => {
+	const { executeRecaptcha } = useGoogleReCaptcha();
+
+	const submit = async () => {
 		console.log(name, email, phone, college, reg);
-		// If success
-		document.getElementById(
-			"message-success"
-		).innerHTML = `Confirmed! You’re in`;
-		// If error
-		document.getElementById(
-			"message-err"
-		).innerHTML = `Something wrong happened try again`;
+		let captcha = await executeRecaptcha("/");
+
+		let url = "https://techchronicle.herokuapp.com/codeInvicta/addUser";
+
+		let data = {
+			name,
+			email,
+			registration_number: reg,
+			mobile_number: phone,
+			campus: college,
+			captcha,
+		};
+
+		try {
+			await axios.post(url, data).then((res) => {
+				// If success
+				document.getElementById(
+					"message-success"
+				).innerHTML = `Confirmed! You’re in`;
+			});
+		} catch (error) {
+			// If error
+			document.getElementById(
+				"message-err"
+			).innerHTML = `Something wrong happened try again`;
+			console.log(error);
+		}
 	};
 
 	return (
@@ -32,6 +54,8 @@ const RegisterSection = () => {
 					behind them.
 				</div>
 				<div>So are you ready for the challenge?</div>
+				<div>We await you -</div>
+
 				<div>Confirm your participation!</div>
 				<div className="mt">
 					<span className="terminal-success">Name:</span>
--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<GoogleReCaptchaProvider
+			reCaptchaKey={process.env.REACT_APP_RECAPTCHA_KEY}
+		>
+			<App />
+		</GoogleReCaptchaProvider>
 	</React.StrictMode>,
 	document.getElementById("root")
 );
