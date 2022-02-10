e8f1c8ed1552470af0a7a6f7e8ff7be75e532367
Sidharth Mishra <sidharth1604@gmail.com>
2022-02-10 18:39:15 +0530

    Card Added

--- src/components/SpeakerSection/SpeakerSection.js
+++ src/components/SpeakerSection/SpeakerSection.js
@@ -1,4 +1,4 @@
-import { Grid } from "@material-ui/core";
+import {Card, CardContent, Typography, Grid} from "@material-ui/core";
 import React from "react";
 import "./SpeakerSection.css";
 
@@ -6,30 +6,53 @@ const SpeakerSection = () => {
 	return (
 		<Grid className="mainDiv" container xs={11} md={8} spacing={2}>
 			<Grid item xs={12}><div className="main-date sp-title">Speakers</div></Grid>
-			<Grid item xs={12} sm={6}>
+			<Grid item xs={12} sm={6} className="card">
+				<Card sx={{ minWidth: 275 }}>
+				<CardContent>
 				<div className="speaker">
 					<img src="/assets/speaker1.jpg" alt="Anoopam Mishra" />
-					<div className="main-date name">Anoopam Mishra</div>
-					<div className="desc">
-						Software Engineer at Google || ICPC Asia West Finalist
-						2020
-					</div>
-					<div className="date">24th March</div>
-					<div className="topic">
+					<Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+						<div className="main-date name Anoopam">Anoopam Mishra</div>
+        			</Typography>
+					<Typography sx={{ fontSize: 14 }}>
+						<div className="desc">
+							Software Engineer at Google
+						</div>
+						<div className="desc">
+						___________________________
+						</div>
+					</Typography>
+					<Typography sx={{ fontSize: 14 }}>
+					<div className="topic desc">
 						Topic- Code Optimisation and debugging
 					</div>
+					<div className="date desc "> <i class="fa fa-calendar"> 24th March 2022</i></div>
+					</Typography>
 				</div>
+				</CardContent>
+				</Card>
 			</Grid>
-			<Grid item xs={12} sm={6}>
-				<div className="speaker">
+			<Grid item xs={12} sm={6} className="card">
+				<Card sx={{ minWidth: 275 }}>
+				<CardContent>
+				<div className="speaker card2">
 					<img src="/assets/speaker2.jpg" alt="Aman Dhattarwal" />
-					<div className="main-date name">Aman Dhattarwal</div>
-					<div className="desc">YouTuber and Entrepreneur</div>
-					<div className="date">25th March</div>
-					<div className="topic">
+					<Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+						<div className="main-date name">Aman Dhattarwal</div>
+        			</Typography>
+					<Typography sx={{ fontSize: 14 }}>
+					<div className="desc1">YouTuber and Entrepreneur</div>
+					<div className="desc1">___________________________</div>
+					</Typography>
+					<Typography sx={{ fontSize: 14 }}>
+					<div className="topic desc1">
 						Topic- Roadmap for Competitive Coding and DSA
 					</div>
+					<div className="date desc1 "> <i class="fa fa-calendar"> 25th March 2022</i></div>
+					</Typography>
 				</div>
+				</CardContent>
+				</Card>
 			</Grid>
 		</Grid>
 	);
