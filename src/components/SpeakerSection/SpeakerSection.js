ec2e006f1a4b6ade198bf1c165f641025002e779
Sidharth Mishra <sidharth1604@gmail.com>
2022-03-04 13:12:29 +0530

    Clouds

--- src/components/SpeakerSection/SpeakerSection.js
+++ src/components/SpeakerSection/SpeakerSection.js
@@ -8,7 +8,9 @@ const SpeakerSection = () => {
 			<Grid item xs={12}><div className="main-date sp-title">Speakers</div></Grid>
 			<Grid item xs={12} sm={6} className="card">
 				<Card sx={{ minWidth: 200 }}>
-				<CardContent style={{backgroundColor: "#4EABDB", backgroundImage: `url(${"public\assets\Slide.png"})` }}>
+				<CardContent style={{
+					backgroundImage: "url('/assets/Slide.png')",
+					backgroundSize: "cover", }}>
 				<div className="speaker">
 					<img src="/assets/speaker1.jpg" alt="Anoopam Mishra" />
 					<Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
@@ -34,7 +36,9 @@ const SpeakerSection = () => {
 			</Grid>
 			<Grid item xs={12} sm={6} className="card">
 				<Card sx={{ minWidth: 275 }}>
-				<CardContent style={{backgroundColor: "#4EABDB"}}>
+				<CardContent style={{
+					backgroundImage: "url('/assets/Slide.png')",
+					backgroundSize: "cover", }}>
 				<div className="speaker card2">
 					<img src="/assets/speaker2.jpg" alt="Aman Dhattarwal" />
 					<Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
