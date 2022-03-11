1e9676f871c71f8feca87b4838fa748fd31925c2
Sidharth Mishra <sidharth1604@gmail.com>
2022-03-11 17:01:29 +0530

    Prizes Added

--- src/components/PrizesSection/PrizesSection.js
+++ src/components/PrizesSection/PrizesSection.js
@@ -6,7 +6,7 @@ const PrizesSection = () => {
 		<>
 			<div className="prizes-section">
 				<div className="grid-section">
-					<p className="prize-title prize-sub prize-rs">&#8377;TBD</p>
+					<p className="prize-title prize-sub prize-rs">&#8377;37K</p>
 					<hr className="prize-divider" />
 					<p className="prize-title prize-sub">Prize Pool</p>
 				</div>
@@ -15,12 +15,12 @@ const PrizesSection = () => {
 						Best Girl Coder
 					</p>
 					<hr className="prize-divider" />
-					<p className="prize-title prize-sub">&#8377;TBD</p>
+					<p className="prize-title prize-sub">&#8377;3,000</p>
 				</div>
 				<div className="grid-section">
-					<p className="prize-title prize-sub">1st Prize: &#8377; TBD</p>
-					<p className="prize-title prize-sub">2nd Prize: &#8377; TBD</p>
-					<p className="prize-title prize-sub">3rd Prize: &#8377; TBD</p>
+					<p className="prize-title prize-sub">1st Prize: &#8377; 15,000</p>
+					<p className="prize-title prize-sub">2nd Prize: &#8377; 8,000</p>
+					<p className="prize-title prize-sub">3rd Prize: &#8377; 5,000</p>
 				</div>
 				{/* <div className="grid-section">
 					<p className="prize-title prize-sub prize-rs">
@@ -30,7 +30,7 @@ const PrizesSection = () => {
 					<p className="prize-title prize-sub">&#8377;TBD</p>
 				</div> */}
 				<div className="grid-section">
-					<p className="prize-title prize-sub prize-rs">&#8377;TBD Each</p>
+					<p className="prize-title prize-sub prize-rs">&#8377;3,000 Each</p>
 					<hr className="prize-divider" />
 					<p className="prize-title prize-sub">Top 2 Freshers</p>
 				</div>
@@ -39,7 +39,7 @@ const PrizesSection = () => {
 						Best VIT Coder
 					</p>
 					<hr className="prize-divider" />
-					<p className="prize-title prize-sub">&#8377;TBD</p>
+					<p className="prize-title prize-sub">&#8377;3,000</p>
 				</div>
 			</div>
 		</>
