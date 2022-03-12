d733b785dff2b0a1c1a7a2617f29b36aab37780a
Sidharth Mishra <sidharth1604@gmail.com>
2022-03-13 01:39:29 +0530

    Prize Updated

--- src/components/PrizesSection/PrizesSection.js
+++ src/components/PrizesSection/PrizesSection.js
@@ -6,7 +6,7 @@ const PrizesSection = () => {
 		<>
 			<div className="prizes-section">
 				<div className="grid-section">
-					<p className="prize-title prize-sub prize-rs">&#8377;37K</p>
+					<p className="prize-title prize-sub prize-rs">&#8377;33K</p>
 					<hr className="prize-divider" />
 					<p className="prize-title prize-sub">Prize Pool</p>
 				</div>
@@ -15,11 +15,11 @@ const PrizesSection = () => {
 						Best Girl Coder
 					</p>
 					<hr className="prize-divider" />
-					<p className="prize-title prize-sub">&#8377;3,000</p>
+					<p className="prize-title prize-sub">&#8377;2,500</p>
 				</div>
 				<div className="grid-section">
-					<p className="prize-title prize-sub">1st Prize: &#8377; 15,000</p>
-					<p className="prize-title prize-sub">2nd Prize: &#8377; 8,000</p>
+					<p className="prize-title prize-sub">1st Prize: &#8377; 10,000</p>
+					<p className="prize-title prize-sub">2nd Prize: &#8377; 7,000</p>
 					<p className="prize-title prize-sub">3rd Prize: &#8377; 5,000</p>
 				</div>
 				{/* <div className="grid-section">
@@ -32,14 +32,14 @@ const PrizesSection = () => {
 				<div className="grid-section">
 					<p className="prize-title prize-sub prize-rs">&#8377;3,000 Each</p>
 					<hr className="prize-divider" />
-					<p className="prize-title prize-sub">Top 2 Freshers</p>
+					<p className="prize-title prize-sub">Top 2 Best VIT Coders</p>
 				</div>
 				<div className="grid-section centertext">
 					<p className="prize-title prize-sub prize-rs">
-						Best VIT Coder
+						Best Fresher
 					</p>
 					<hr className="prize-divider" />
-					<p className="prize-title prize-sub">&#8377;3,000</p>
+					<p className="prize-title prize-sub">&#8377;2,500</p>
 				</div>
 			</div>
 		</>
